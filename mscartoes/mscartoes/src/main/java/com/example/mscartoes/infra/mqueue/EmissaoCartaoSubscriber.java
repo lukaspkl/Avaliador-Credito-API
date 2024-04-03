d4b88c43cfe78058a7c2d17ce90724bc61ca288a
lukaspkl <fohrenoficial@Live.com>
2024-04-02 23:13:13 -0300

    adicionando Log no MS cartão

--- mscartoes/mscartoes/src/main/java/com/example/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
+++ mscartoes/mscartoes/src/main/java/com/example/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
@@ -8,12 +8,14 @@ import com.example.mscartoes.infra.repository.ClienteCartaoRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
+@Slf4j
 public class EmissaoCartaoSubscriber {
 
     private final CartaoRepository cartaoRepository;
@@ -35,7 +37,7 @@ public class EmissaoCartaoSubscriber {
             clienteCartaoRepository.save(clienteCartao);
 
         }catch (Exception e){
-          e.printStackTrace();
+          log.error("Erro ao receber solicitação de emissão de cartão: {}", e.getMessage());
         }
 
     }
